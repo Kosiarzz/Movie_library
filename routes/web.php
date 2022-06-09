1331ad480dc5ffed49295d5759bc021a84d781c8
PatrykP <patryk.pasciakk@gmail.com>
2022-06-09 17:10:57 +0200

    Creation of auth middleware for selected pages

--- routes/web.php
+++ routes/web.php
@@ -16,35 +16,38 @@ use Illuminate\Support\Facades\Route;
 
 Route::redirect('/', '/home');
 
-Route::get('/home', [HomeController::class, 'homeView'])->name('home');
-Route::get('/biblioteka', [HomeController::class, 'libraryView'])->name('library');
+Route::middleware(['auth','verified'])->group(function()
+{
+    Route::get('/home', [HomeController::class, 'homeView'])->name('home');
+    Route::get('/biblioteka', [HomeController::class, 'libraryView'])->name('library');
 
-Route::get('/profil', [ProfileController::class, 'profileView'])->name('profile');
-Route::post('/profil/update', [ProfileController::class, 'updateProfileData'])->name('updateProfileData');
-Route::post('/profil/updatePassword', [ProfileController::class, 'updatePassword'])->name('updatePassword');
+    Route::get('/profil', [ProfileController::class, 'profileView'])->name('profile');
+    Route::post('/profil/update', [ProfileController::class, 'updateProfileData'])->name('updateProfileData');
+    Route::post('/profil/updatePassword', [ProfileController::class, 'updatePassword'])->name('updatePassword');
 
-Route::get('/filmy', [ScraperController::class, 'index'])->name('viewScrapMovies');
-Route::get('/filmy/wyszukiwanie', [ScraperController::class, 'getMovies'])->name('scrapMovies');
+    Route::get('/filmy', [ScraperController::class, 'index'])->name('viewScrapMovies');
+    Route::get('/filmy/wyszukiwanie', [ScraperController::class, 'getMovies'])->name('scrapMovies');
 
-Route::post('/grupa/dodawanie', [GroupController::class, 'store'])->name('addGroup');
-Route::get('/grupa/szczegoly/{id}', [GroupController::class, 'show'])->name('groupShow');
-Route::get('/grupa/usuwanie/{id}', [GroupController::class, 'destroy'])->name('groupDestroy');
-Route::post('/grupa/update', [GroupController::class, 'update'])->name('groupUpdate');
+    Route::post('/grupa/dodawanie', [GroupController::class, 'store'])->name('addGroup');
+    Route::get('/grupa/szczegoly/{id}', [GroupController::class, 'show'])->name('groupShow');
+    Route::get('/grupa/usuwanie/{id}', [GroupController::class, 'destroy'])->name('groupDestroy');
+    Route::post('/grupa/update', [GroupController::class, 'update'])->name('groupUpdate');
 
-Route::post('/film/dodawanie', [MovieController::class, 'storeAjaxMovie'])->name('addMovie');
-Route::get('/film/szczegoly/{id}', [MovieController::class, 'show'])->name('movieShow');
-Route::get('/film/edycja/{id}', [MovieController::class, 'edit'])->name('editMovie');
-Route::post('/film/update', [MovieController::class, 'update'])->name('updateMovie');
-Route::get('/film/usun/{id}', [MovieController::class, 'destroy'])->name('deleteMovie');
-Route::get('/film/filtry/usun/{id}', [MovieController::class, 'destroyFromFilters'])->name('deleteMovieFromFilters');
-Route::get('/film/nowy', [MovieController::class, 'create'])->name('createMovie');
-Route::post('/film/dodawanie/niestandardowy', [MovieController::class, 'storeCustomMovie'])->name('storeCustomMovie');
-Route::post('/film/dodawanie/scrap/niestandardowy', [MovieController::class, 'storeCustomMovie'])->name('storeScrapCustomMovie');
+    Route::post('/film/dodawanie', [MovieController::class, 'storeAjaxMovie'])->name('addMovie');
+    Route::get('/film/szczegoly/{id}', [MovieController::class, 'show'])->name('movieShow');
+    Route::get('/film/edycja/{id}', [MovieController::class, 'edit'])->name('editMovie');
+    Route::post('/film/update', [MovieController::class, 'update'])->name('updateMovie');
+    Route::get('/film/usun/{id}', [MovieController::class, 'destroy'])->name('deleteMovie');
+    Route::get('/film/filtry/usun/{id}', [MovieController::class, 'destroyFromFilters'])->name('deleteMovieFromFilters');
+    Route::get('/film/nowy', [MovieController::class, 'create'])->name('createMovie');
+    Route::post('/film/dodawanie/niestandardowy', [MovieController::class, 'storeCustomMovie'])->name('storeCustomMovie');
+    Route::post('/film/dodawanie/scrap/niestandardowy', [MovieController::class, 'storeCustomMovie'])->name('storeScrapCustomMovie');
 
-Route::get('/home/{id}', [FilterController::class, 'genreFilter'])->name('genreFilter');
-Route::get('/filtry', [FilterController::class, 'filters'])->name('filters');
-Route::get('/filtry/{title}', [FilterController::class, 'filters'])->name('scrapToFilters');
-Route::get('/filtry/kategoria/{category}', [FilterController::class, 'filters'])->name('searchByCategory');
-Route::get('/filtry/grupa/{group}', [FilterController::class, 'filters'])->name('searchByGroup');
+    Route::get('/home/{id}', [FilterController::class, 'genreFilter'])->name('genreFilter');
+    Route::get('/filtry', [FilterController::class, 'filters'])->name('filters');
+    Route::get('/filtry/{title}', [FilterController::class, 'filters'])->name('scrapToFilters');
+    Route::get('/filtry/kategoria/{category}', [FilterController::class, 'filters'])->name('searchByCategory');
+    Route::get('/filtry/grupa/{group}', [FilterController::class, 'filters'])->name('searchByGroup');
+});
 
 Auth::routes();
