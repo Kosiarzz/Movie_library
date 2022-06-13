a09d073b4bb1620856149520086b257d775a63f7
PatrykP <patryk.pasciakk@gmail.com>
2022-06-13 19:40:58 +0200

    Unit test the movie search on the scraping page

---
+++ tests/Feature/ScrapTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Models\User;
+
+class ScrapTest extends TestCase
+{
+    /**
+     * Testing the movie search on the scraping page.
+     *
+     * @return void
+     */
+    public function test_search_movie_scrap()
+    {
+        $user = User::firstOrCreate([
+            'name' => 'test',
+            'email' => 'test@test.test',
+            'password' => 'test'
+        ]);
+
+        $this->actingAs($user);
+
+        $movieName = "Avengers";
+
+        $response = $this->get('/filmy/wyszukiwanie?name='. $movieName);
+
+        $response->assertStatus(200)->assertSeeText($movieName);
+    }
+}
